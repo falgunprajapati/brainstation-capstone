d80205e40fec8cb49e110bc2f00e870d48b898f5
falgunprajapati <falgunprajapati8@gmail.com>
2022-12-22 03:59:37 -0500

    created ticket ,notes ,new tickets ,register modules

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import { ToastContainer } from "react-toastify"
 import "react-toastify/dist/ReactToastify.css"
 import Header from "./components/Header"
+import PrivateRoute from "./components/PrivateRoute"
 import Home from "./pages/Home"
 import Login from "./pages/Login"
 import Register from "./pages/Register"
+import NewTicket from "./pages/NewTicket"
+import Tickets from "./pages/Tickets"
+import Ticket from "./pages/Ticket"
+
 function App() {
   return (
     <>
@@ -12,9 +17,33 @@ function App() {
         <div className="container">
           <Header />
           <Routes>
-            <Route path="/" element={<Home />}></Route>
-            <Route path="/login" element={<Login />}></Route>
-            <Route path="/register" element={<Register />}></Route>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route
+              path="/new-ticket"
+              element={
+                <PrivateRoute>
+                  <NewTicket />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/tickets"
+              element={
+                <PrivateRoute>
+                  <Tickets />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/ticket/:ticketId"
+              element={
+                <PrivateRoute>
+                  <Ticket />
+                </PrivateRoute>
+              }
+            />
           </Routes>
         </div>
       </Router>
